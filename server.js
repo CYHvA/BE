b34a359e82fff5478b7b5ddca496b70ec82e33f5
CyHvA <chunyin.lai@hva.nl>
2022-05-16 10:48:12 +0200

    port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require("path");
 
 //Template engine
@@ -8,31 +8,11 @@ app.set("view engine", "ejs");
 
 //Render template engine route to index page
 app.get("/", (req, res) => {
-  res.render("index", { title: "Hey", message: "Hello there!" });
+  res.render("pages/index", { title: "Hey", message: "Hello there!" });
 });
 
 //Linked assets folder to local server with route prefix
-app.use("/public", express.static("public"));
-
-//Route to index
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-//Route to About
-app.get("/about", (req, res) => {
-  res.send("This is the about page.");
-});
-
-//Route to Login
-app.get("/login", (req, res) => {
-  res.send("This is the login page.");
-});
-
-//Route to test
-app.get("/test", (req, res) => {
-  res.send("This is the test.");
-});
+app.use("/static", express.static("static"));
 
 //Error 404
 app.use((req, res, next) => {
