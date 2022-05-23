c2adbaf5ab70dd009d43e96cdd607bdb33356600
CyHvA <chunyin.lai@hva.nl>
2022-05-23 11:37:05 +0200

    new page

--- server.js
+++ server.js
@@ -1,25 +1,97 @@
+const { name } = require("ejs");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
+// const dotenv = require("dotenv").config();
+// const { MongoClient } = require("mongodb");
+// const { ObjectId } = require("mongodb");
 
-//Template engine
+// async function connectDB() {
+//   const uri = process.env.DB_URI;
+
+//   const client = new MongoClient(uri, {
+//     useNewUrlParser: true,
+
+//     useUnifiedTopology: true,
+//   });
+
+//   try {
+//     await client.connect();
+
+//     db = client.db(process.env.DB_NAME);
+//   } catch (error) {
+//     throw error;
+//   }
+// }
+
+/*********************************************
+ * Data
+ *********************************************/
+const songsData = require("./data/mockdata.js");
+
+/*********************************************
+ * Temporarily Mock Data
+ *********************************************/
+
+const songs = [
+  {
+    id: 1,
+    slug: "nun-to-me",
+    name: "Nun To Me",
+    artist: "Kankan",
+    year: "2020",
+    genre: ["hip-hop", "underground rap"],
+  },
+  {
+    id: 2,
+    slug: "so-much-cheese",
+    name: "So Much Cheese",
+    artist: "Summrs",
+    year: "2022",
+    genre: ["hip-hop", "underground rap"],
+  },
+];
+
+/*********************************************
+ * Template Engine
+ *********************************************/
 app.set("view engine", "ejs");
 
-//Render template engine route to index page
-app.get("/", (req, res) => {
-  res.render("pages/index", { title: "Hey", message: "Hello there!" });
+/*********************************************
+ * Pages
+ *********************************************/
+// app.get("/", (req, res) => {
+
+//   let doc = '
+//   songs.forEach( name => {
+//     res.send(`<h1>${songs.name}</h1>`)
+//   })
+// });
+
+app.get("/home", (req, res) => {
+  res.render("pages/index", {});
 });
 
-//Linked assets folder to local server with route prefix
+app.get("/addsong", (req, res) => {
+  res.render("pages/form", {});
+});
+
+/*********************************************
+ * Middleware
+ *********************************************/
 app.use("/static", express.static("static"));
 
-//Error 404
+/*********************************************
+ * Error 404
+ *********************************************/
 app.use((req, res, next) => {
   res.status(404).send("Error 404, Sorry can't find that!");
 });
 
-//Listens to a certain port (currently on port: 3000)
+/*********************************************
+ * Listens to a certain port (currently on port: 3000)
+ *********************************************/
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
