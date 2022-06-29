4312f8b76408eaa49d59553cf2da48a87f798b62
CyHvA <chunyin.lai@hva.nl>
2022-06-29 21:30:04 +0200

    final api fix

--- static/js/image.js
+++ static/js/image.js
@@ -1,29 +1,48 @@
-// Link zetten met de ID "thumbnail"
-const thumbNail = document.getElementById('thumbnail')
+// // Link zetten met de ID "thumbnail"
+// const thumbNail = document.getElementById('thumbnail')
 
-// Link zetten met de "default" img (Fallback)
-const fbImg = document.querySelector('.img')
+// // Link zetten met de "default" img (Fallback)
+// const fbImg = document.querySelector('.img')
 
-// De image verwijderen als Javascript aan staat
-function fallBack(){
-  fbImg.remove();
-};
+// // De image verwijderen als Javascript aan staat
+// function fallBack(){
+//   fbImg.remove();
+// };
 
-fallBack()
+// fallBack()
 
-// Ophalen van API data 
-fetch('https://place.dog/200/200')
+// // Ophalen van API data 
+// fetch('https://place.dog/200/200')
+//   // .then( res => console.log(res))
+
+//   // Deze API hoeft niet omgezet te worden naar een .json omdat het al een .json API is
+//   // De "thumbNail" vullen met de "data"(API) en geef ik de URL mee
+//   .then( data => {
+//     thumbNail.innerHTML = `<img src="${data.url}"/>`
+//   })
+
+//   // Hiermee catch ik de error op als er een error is.
+//   .catch( error => console.error(`Error fetching message: ${error.message}`)
+//   );
+
+
+const sections = document.querySelectorAll('section.blocks')
+
+sections.forEach(section => {
+  const image = section.children[0];
+  image.remove();
+  const thumb = section.children[0];
+  fetch('https://place.dog/200/200')
   // .then( res => console.log(res))
 
   // Deze API hoeft niet omgezet te worden naar een .json omdat het al een .json API is
   // De "thumbNail" vullen met de "data"(API) en geef ik de URL mee
   .then( data => {
-    thumbNail.innerHTML = `<img src="${data.url}"/>`
+    thumb.innerHTML = `<img src="${data.url}"/>`
   })
 
   // Hiermee catch ik de error op als er een error is.
   .catch( error => console.error(`Error fetching message: ${error.message}`)
   );
-
-
+});
   
