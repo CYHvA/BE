c3e9289c19fe5c319befdee44339aa2f821577e4
CyHvA <chunyin.lai@hva.nl>
2022-06-29 21:32:54 +0200

    removed unused commands and comments

--- static/js/image.js
+++ static/js/image.js
@@ -1,39 +1,15 @@
-// // Link zetten met de ID "thumbnail"
-// const thumbNail = document.getElementById('thumbnail')
+const blocks = document.querySelectorAll('section.blocks')
 
-// // Link zetten met de "default" img (Fallback)
-// const fbImg = document.querySelector('.img')
-
-// // De image verwijderen als Javascript aan staat
-// function fallBack(){
-//   fbImg.remove();
-// };
-
-// fallBack()
-
-// // Ophalen van API data 
-// fetch('https://place.dog/200/200')
-//   // .then( res => console.log(res))
-
-//   // Deze API hoeft niet omgezet te worden naar een .json omdat het al een .json API is
-//   // De "thumbNail" vullen met de "data"(API) en geef ik de URL mee
-//   .then( data => {
-//     thumbNail.innerHTML = `<img src="${data.url}"/>`
-//   })
-
-//   // Hiermee catch ik de error op als er een error is.
-//   .catch( error => console.error(`Error fetching message: ${error.message}`)
-//   );
-
-
-const sections = document.querySelectorAll('section.blocks')
-
-sections.forEach(section => {
+// Voor elke section binnen class block
+blocks.forEach(section => {
+  //Aanwijzen van de 1e foto en die weghalen
   const image = section.children[0];
   image.remove();
+
+  //Omdat de eerste foto is verwijdered, is de telling weer op 0 en word de API geplaatst
   const thumb = section.children[0];
+
   fetch('https://place.dog/200/200')
-  // .then( res => console.log(res))
 
   // Deze API hoeft niet omgezet te worden naar een .json omdat het al een .json API is
   // De "thumbNail" vullen met de "data"(API) en geef ik de URL mee
